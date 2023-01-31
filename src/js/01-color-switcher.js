41e01a25980d58433a572589427bef0c90a95ac0
PlutovaKate <114903556+PlutovaKate@users.noreply.github.com>
2023-01-31 22:13:48 +0300

    первое и второе задания

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let intervalId = null;
+
+function changeBodyColor(){
+    intervalId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.setAttribute("disabled", true)
+    startBtn.disabled = true;
+}
+
+function stopChange(){
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', changeBodyColor);
+stopBtn.addEventListener('click', stopChange);
